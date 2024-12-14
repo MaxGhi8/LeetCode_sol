51868bd82c27262436932c194ffebe680c1c3128
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2024-12-14 13:29:53 +0100

    optimized

--- python/LeetCode26.py
+++ python/LeetCode26.py
@@ -1,11 +1,20 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 1
-        while True:
-            try:
-                if nums[i] == nums[i - 1]:
-                    nums.pop(i)
-                else:
-                    i += 1
-            except:
-                return i
+        ## This version in not optimized due to the pop operation
+        # i = 1
+        # while True:
+        #     try:
+        #         if nums[i] == nums[i - 1]:
+        #             nums.pop(i)
+        #         else:
+        #             i += 1
+        #     except:
+        #         return i
+
+        ## This version is optimized (without pop operation)
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
